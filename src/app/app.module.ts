2eea9c0a47d8a96b3aa4fda87adefbfe10ea21d9
jose correa <jorenecp10@gmail.com>
2023-01-17 11:13:53 -0500

    estudio httpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { HttpClientTestComponent } from './http-client-test/http-client-test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HttpClientTestComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
